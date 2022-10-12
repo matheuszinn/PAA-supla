32d93b1b16aa2019ce283efa9cb5051ea8387c2c
BrunoHibou <brunocavalcanticosta.bc@gmail.com>
2022-10-12 18:14:05 -0300

    comentarios

--- bubble.py
+++ bubble.py
@@ -16,27 +16,32 @@ def generate_list(size):
         list.append(random.randint(1, 1000))
     return list
 
-
+#função de busca
 def bubble_sort(list):
-    old_list = copy.deepcopy(list)
-    num_trocas = 0
-    num_comp = 0
+
+    
+    old_list = copy.deepcopy(list) #lista antiga
+    num_trocas = 0 #armazena a quantidade de trocas realizadas
+    num_comp = 0 #armazena a quantidade de comparações realizadas
+    
+    #actual sorting
     while True:
         did_swap = False
         for index, value in enumerate(list):
 
             if (index != len(list)-1) and ((value) > list[index+1]):
-                list[index] = list[index+1]
-                list[index+1] = value
-                num_trocas = num_trocas + 1
+                list[index] = list[index+1] #trocas 
+                list[index+1] = value       
+                num_trocas = num_trocas + 1 #stats
                 if did_swap != True:
                     did_swap = True
-            num_comp = num_comp + 1
+            num_comp = num_comp + 1 #stats
         if did_swap == False:
             break
 
-    print(old_list)
-    print(list)
+    #impressão das estatísticas
+    #print(old_list) lista antiga para testes
+    print(list)#lista atual. NÁO IMPRIMA AS LISTAS PARA TAMANHOS MUITO GRANDES
     print("trocas: {} \ncomparações: {}".format(num_trocas, num_comp))
 
 
@@ -50,6 +55,7 @@ if __name__ == '__main__':
     # realiza o calculo do tempo percorrido
     execution_time = time.time() - start_time
 
+    #imprime outras estatísticas
     print("tempo: {}".format(execution_time))
     print("tamanho: {} bytes".format(sys.getsizeof(list)))
     print('feito!')
