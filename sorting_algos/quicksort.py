fc0a05247eb2e7cf7c5c1acd64db318d8e25570b
matheuszinn <theustloz@gmail.com>
2022-10-20 22:35:49 -0300

    ajeitando o quick|estava dando erro de recursao

--- sorting_algos/quicksort.py
+++ sorting_algos/quicksort.py
@@ -3,36 +3,45 @@ import math
 import time
 from random import shuffle
 from typing import List
+from random import randrange
 
 
 class Quicksort:
-
     def __init__(self):
         self.comparacoes = 0
         self.troca = 0
 
     def quicksort(self, A, low=0, high=None):
         start_time = time.time()
+        high = high if high is not None else len(A)
         if low < high:
             part = self.particao(A, low, high)
-            self.quicksort(A, low, part - 1)
+            self.quicksort(A, low, part)
             self.quicksort(A, part + 1, high)
             self.comparacoes += 1
 
         execution_time = time.time() - start_time
-        return A, {"time": execution_time, "comparacoes": self.comparacoes, "troca": self.troca}
+        return A, {
+            "time": execution_time,
+            "comparacoes": self.comparacoes,
+            "troca": self.troca,
+        }
 
     def particao(self, A, low, high):
-        pivot = A[high]
-        i = low - 1
+        r = randrange(low, high)
+        A[r], A[low - 1] = A[low - 1], A[r]
+        self.troca += 1
+        pivo = A[high - 1]
+        for i in range(low, high):
+            if A[i] > pivo:
+                high += 1
+            else:
+                high += 1
+                low += 1
+                A[i], A[low - 1] = A[low - 1], A[i]
+                self.troca += 1
+            self.comparacoes += 1
+        return low - 1
 
-        for j in range(low, high):
-            if A[j] <= pivot:
-                i = i + 1
-                (A[i], A[j]) = (A[j], A[i])
-                self.comparacoes += 1
-            self.troca += 1
-
-        (A[i + 1], A[high]) = (A[high], A[i + 1])
-        self.comparacoes += 1
-        return i + 1
+import random
+print(Quicksort().quicksort([random.randint(1,1000) for _ in range(1000)])[1])
